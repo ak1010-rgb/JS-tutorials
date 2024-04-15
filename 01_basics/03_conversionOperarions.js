99824002134d87a3e17eb7fd383cf012fd0a1e79
APOORV YADAV <121373491+ak1010-rgb@users.noreply.github.com>
2024-04-15 06:09:12 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperarions.js
@@ -0,0 +1,59 @@
+let score = "apoorv"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//note: "33" => 33
+//"33abc" =>NaN
+//true =>1, false=>0
+let isLoggedIn="apoorv"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=> false
+// null""=>false
+//"apoorv"=>true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************OPERATIONS********************
+let value =3;
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+//add two or more strings
+let str1 ="hello"
+let str2 =" apoorv"
+let str3= str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study conversion:https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
